4475bd3dc6479838a06c0738411976ae874d5503
Jose Pedro Valentim Oliveira <zeca1997@hotmail.com>
2020-03-12 17:00:29 +0100

    v1

---
+++ public/js/gen8.js
@@ -0,0 +1,39 @@
+let dex = document.getElementById("dex");
+let counter = 1;
+let box, boxes = 14;
+
+
+    for (let index = 1; index <= 178; index++) {
+
+        if (index == 1 || counter == 30) {
+            counter = 1;
+
+            box = document.createElement('div');
+            box.className = "box";
+            let foto = document.createElement('img');
+            foto.className = "sprite";
+
+            foto.src = "images/sprites/gen 8/0" + index + ".png";
+
+            box.appendChild(foto);
+            dex.appendChild(box);
+
+        }
+        else {
+            let foto = document.createElement('img');
+            foto.className = "sprite";
+
+            foto.src = "images/sprites/gen 8/0" + index + ".png";
+
+            box.appendChild(foto);
+            dex.appendChild(box);
+            counter++;
+        }
+
+
+
+
+    
+
+
+}
