f8cdacedac8f9e63212c928b8c0f9508c357bc14
Jose Pedro Valentim Oliveira <zeca1997@hotmail.com>
2020-03-30 00:06:41 +0200

    gen1 added

--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -1,3 +1,4 @@
+
 let dex = document.getElementById("dex");
 let counter = 0;
 let box = document.createElement('div');
@@ -21,25 +22,55 @@ function checkDex() {
         } else if (regularDex) {
             type = "regular";
         }
-        
+
+
         box.textContent = "";
-        displayDex(data, gender, type,region,bothDex);
+        displayDex(data, gender, type, region, bothDex);
     });
 }
+
 
 function displayPokemon(element, type, extra) {
     let foto = document.createElement('img');
     foto.className = "sprite";
     foto.src = "images/sprites/" + type + "/" + element.id + extra + ".png";
+    foto.addEventListener('click', function () {
+        
+        let info = document.getElementById('info');
+        let dex = document.getElementById('dex');
+        let name = document.createElement('p');
+        let btn = document.createElement('button');
+        let fotoInfo = document.createElement('img');
+        btn.textContent = 'close';
+        btn.addEventListener('click',function() {
+            info.textContent ="";
+            info.style.width = "0%";
+            dex.style.width = "100%";
+        });
+        info.textContent = "";
+        fotoInfo.src = foto.src;
+        name.textContent = element.name;
+        name.style.fontSize = "40px";
+        info.style.width = "20%";
+        dex.style.width = "80%";
+        
+
+
+        info.appendChild(name);
+        info.appendChild(fotoInfo);
+        info.appendChild(btn);
+    });
+
+
     box.appendChild(foto);
 }
 
 // display pokemon
-function displayDex(data, gender, type,region,bothDex) {
+function displayDex(data, gender, type, region, bothDex) {
 
     data.forEach(element => {
 
-        if(bothDex){
+        if (bothDex) {
             displayPokemon(element, "regular", "");
             displayPokemon(element, "shiny", "");
             if (gender) {
@@ -48,36 +79,36 @@ function displayDex(data, gender, type,region,bothDex) {
                     displayPokemon(element, "shiny", "-f");
                 }
             }
-            if(region){
-            if (element.regionForm) {
-                if (element.region.alola) {
-                    displayPokemon(element, "regular", "-a");
-                    displayPokemon(element, "shiny", "-a");
+            if (region) {
+                if (element.regionForm) {
+                    if (element.region.alola) {
+                        displayPokemon(element, "regular", "-a");
+                        displayPokemon(element, "shiny", "-a");
+                    }
+                    if (element.region.galar) {
+                        displayPokemon(element, "regular", "-g");
+                        displayPokemon(element, "shiny", "-g");
+                    }
                 }
-                if (element.region.galar) {
-                    displayPokemon(element, "regular", "-g");
-                    displayPokemon(element, "shiny", "-g");
+            }
+        } else {
+            displayPokemon(element, type, "");
+            if (gender) {
+                if (element.genderForm) {
+                    displayPokemon(element, type, "-f");
                 }
-            }}
-        }
-        else{
-
-
-        displayPokemon(element, type, "");
-        if (gender) {
-            if (element.genderForm) {
-                displayPokemon(element, type, "-f");
+            }
+            if (region) {
+                if (element.regionForm) {
+                    if (element.region.alola) {
+                        displayPokemon(element, type, "-a")
+                    }
+                    if (element.region.galar) {
+                        displayPokemon(element, type, "-g")
+                    }
+                }
             }
         }
-        if(region){
-        if (element.regionForm) {
-            if (element.region.alola) {
-                displayPokemon(element, type, "-a")
-            }
-            if (element.region.galar) {
-                displayPokemon(element, type, "-g")
-            }
-        }}}
     });
     dex.appendChild(box);
 }
